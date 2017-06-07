f79be395e7ccc5d2643a1bd15148929a88021757
Gicela <gicela@mexicanwave.comq>
2017-06-07 21:07:10 +0100

    Implements new game

--- app.js
+++ app.js
@@ -8,27 +8,18 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+init();
 
-
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = 0;
-document.getElementById('score-1').textContent = 0;
-document.getElementById('current-0').textContent = 0;
-document.getElementById('current-1').textContent = 0;
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
-    //1.random number
+    if (gamePlaying){
+ //1.random number
     var dice = Math.floor(Math.random() * 6) + 1;
 
     //2. Display the result
-    var diceDOM =     document.querySelector('.dice');
+    var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
 
@@ -43,10 +34,13 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         //Next player
        nextPlayer();
     }
+}  
+    
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function(){
-    //add current score to global score 
+    if (gamePlaying)
+        //add current score to global score 
     scores[activePlayer] += roundScore;
 
     //Update the UI
@@ -59,12 +53,12 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
         document.querySelector('.dice').style.display = 'none';
         document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
         document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-      
+        gamePlaying = false;
+
     } else {
      //Next Player
      nextPlayer();
     }
-
 
 });
 
@@ -79,9 +73,30 @@ function nextPlayer(){
         document.querySelector('.player-1-panel').classList.toggle('active');
 
         document.querySelector('.dice').style.display = 'none';
-}
+};
 
+document.querySelector('.btn-new').addEventListener('click', init);
 
+function init(){
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+    gamePlaying = true;
+    
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = 0;
+    document.getElementById('score-1').textContent = 0;
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+
+};
         
        // document.querySelector('.player-0-panel').classList.remove('active');
         //document.querySelector('.player-1-panel').classList.add('active');
